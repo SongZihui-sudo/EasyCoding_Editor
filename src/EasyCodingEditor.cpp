3d28e3ec0c7246d91d751ee4515f09d17a5fd206
SongZihui-sudo <1751122876@qq.com>
2022-01-29 13:23:34 +0800

    [bug]解决保存文件失败的bug

--- src/EasyCodingEditor.cpp
+++ src/EasyCodingEditor.cpp
@@ -506,6 +506,7 @@ bool easyhtmleditor::creat_files(){
     }
     return true;
 }
+//保存
 bool easyhtmleditor::save_files(string filename,deque < deque <string> > save_Data){
     ofstream in;
     in.open(filename);
